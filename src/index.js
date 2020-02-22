16f29c301770d47a9f5cd23caf9db7adabccf5a8
Jose Bolufer <jose.bolufer@arcadiagroup.co.uk>
2020-02-22 22:28:01 +0000

    updated the port number

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 var cors = require('cors')
 const app = express()
-const port = 2000
+const port = process.env.PORT || 2000
 
 app.use(cors())
 app.use(bodyParser.json())
