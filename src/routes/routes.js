b78ca3c55f5bc9717f7dd3f64c56fbc5e517ac3b
Jose Bolufer <jose.bolufer@arcadiagroup.co.uk>
2020-03-29 17:20:33 +0100

    removed connected routes to debug

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,14 @@
-const db = require('../queries')
+//const db = require('../queries')
 
 module.exports = app => {
   app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
   })
-  app.get('/users', db.getUsers)
-  app.get('/users/:id', db.getUserById)
-  app.post('/users', db.createUser)
-  app.put('/users/:id', db.updateUser)
-  app.delete('/users/:id', db.deleteUser)
+  // app.get('/users', db.getUsers)
+  // app.get('/users/:id', db.getUserById)
+  // app.post('/users', db.createUser)
+  // app.put('/users/:id', db.updateUser)
+  // app.delete('/users/:id', db.deleteUser)
 
   // 404 errors
   app.use((req, res, next) => {
